db136c9fd7a41cd977ee01a11eb40505f1485525
AravindDisha <daravind3@gatech.edu>
2022-09-24 14:22:14 -0400

    Changes to accept date range

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def index():
     print('Request for index page received')
     return render_template('index.html')
 
+
 @app.route('/fetch', methods=['POST'])
 def fetch():
     print('Request for data fetch received')
@@ -33,13 +34,20 @@ def fetch():
     start_date = request.form.get('start_date')
     end_date = request.form.get('end_date')
     if extraction_type:
-        print('Request for fetch page received with extraction_type=%s' %
-              extraction_type)
-        print('start_date=%s' % start_date)
-        print('end_date=%s' % end_date)
-        url_for_extraction = '#'
+        print('Request for fetch page received with:')
+        print('extraction_type=%s' % extraction_type)
+        print('start_date="%s"' % start_date)
+        print('end_date="%s"' % end_date)
+        url_for_extraction = '/'
+
+        if (end_date == ''):
+            end_date = datetime.now().strftime("%Y-%m-%d")
+        if (start_date == ''):
+            start_date = (datetime.now() - relativedelta(days=1)
+                          ).strftime("%Y-%m-%d")
+
         if (extraction_type == 'tweet_keyword'):
-            url_for_extraction = url_for('collect_tweets_keyword')
+            url_for_extraction = url_for('collect_tweets_keyword',start_date=start_date,end_date=end_date)
         return render_template('hello.html', etype=extraction_type, sdate=start_date, edate=end_date, url_for_extraction=url_for_extraction)
     else:
         print('Request for extraction invalid -- redirecting')
@@ -49,20 +57,12 @@ def fetch():
 @app.route('/fetchTweetsKeywords', methods=['GET'])
 def collect_tweets_keyword():
     # collecting args of GET request to get start and end dates of tweets to be collected
-    dates = request.args.to_dict()
-    if 'end_date' in dates.keys() and dates['end_date']!= '':
-        # edit according to incoming format
-        end_date = datetime.strptime(dates['end_date'], '%Y-%m-%d').date()
-    else:
-        end_date = datetime.now()
+    args = request.args
+    end_date = datetime.strptime(args.get('amp;end_date'), "%Y-%m-%d") # amp; is a temporary fix due to string conversion errors
+    start_date = datetime.strptime(args.get('start_date'), "%Y-%m-%d")
+    print('Extraction of tweet by keyword from started')
 
-    if 'start_date' in dates.keys() and dates['start_date']!= '':
-        # edit according to incoming format
-        start_date = datetime.strptime(dates['start_date'], '%Y-%m-%d').date()
-    else:
-        start_date = end_date - relativedelta(days=7)
-
-    # from here - move to a concurrent thread using ThreadPoolExecutor and send output as log. Assign Job IDs? 
+    # from here - move to a concurrent thread using ThreadPoolExecutor and send output as log. Assign Job IDs?
     # naming for file to store extraction from a certain date to a certain date
     path_to_storage_file = path_to_new_extractions.format(
         sm_type="tweet_keyword", from_date=start_date.timestamp(), to_date=end_date.timestamp())
