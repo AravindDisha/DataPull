8bf822500253248d789c435888398e4d44907997
AravindDisha <daravind3@gatech.edu>
2022-10-06 01:04:32 -0400

    Adding comment extraction and basic batching

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import importlib
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for
 from twitter_fetch import get_twitter_posts_from_lists, get_twitter_posts_with_location
+import exportcomments_fetch
+from exportcomments_fetch import get_post_comments
+importlib.reload(exportcomments_fetch)
 from ct_fetch import get_insta_posts, get_fb_posts
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 from pathlib import Path
 import traceback
-import os
 import pickle
 # use concurrent.futures to parallelize
 
@@ -14,9 +17,12 @@ home = Path.home()
 path_to_ct_keys_yaml = './files/crowdtangle_keys.yaml'
 path_to_twit_keys_yaml = './files/twitter_keys.yaml'
 path_to_keywords_yaml = "./files/keywords.yaml"
+path_to_exportcomments_keys = './files/exportcomments_keys.yaml'
 path_to_new_extractions = "/PulledData/data_{sm_type}_{from_date}_{to_date}.pkl"
-# comment this before pushing
-# path_to_new_extractions = "./Data/data_{sm_type}_{from_date}_{to_date}.pkl"
+path_to_pre_comment_extractions = "/PulledData/Raw/data_{sm_type}_{from_date}_{to_date}.pkl"
+
+# path_to_new_extractions = "./Data/data_{sm_type}_{from_date}_{to_date}.pkl" # comment this before pushing
+# path_to_pre_comment_extractions = "./Data/Raw/data_{sm_type}_{from_date}_{to_date}.pkl" # comment this before pushing
 
 app = Flask(__name__)
 
@@ -43,7 +49,7 @@ def fetch():
         if (end_date == ''):
             end_date = datetime.now().strftime("%Y-%m-%d")
         if (start_date == ''):
-            start_date = (datetime.now() - relativedelta(days=1)
+            start_date = (datetime.now() - relativedelta(days=7) #change days to 7 before uploading
                           ).strftime("%Y-%m-%d")
 
         if (extraction_type == 'tweet_keyword'):
@@ -52,6 +58,8 @@ def fetch():
             url_for_extraction = url_for('collect_insta_keyword',start_date=start_date,end_date=end_date)
         elif (extraction_type == 'fb_post'):
             url_for_extraction = url_for('collect_facebook_keyword',start_date=start_date,end_date=end_date)
+        elif (extraction_type == 'tweet_location'):
+            url_for_extraction = url_for('collect_tweet_location',start_date=start_date,end_date=end_date)
         return render_template('hello.html', etype=extraction_type, sdate=start_date, edate=end_date, url_for_extraction=url_for_extraction)
     else:
         print('Request for extraction invalid -- redirecting')
@@ -74,6 +82,7 @@ def collect_tweets_keyword():
     try:
         twit_df = get_twitter_posts_from_lists(
             path_to_keywords_yaml, path_to_twit_keys_yaml, start_date, end_date)
+        print("Dumping data to pkl")
         f = open(path_to_storage_file, 'wb')
         pickle.dump(twit_df, f)
         f.close()
@@ -90,6 +99,38 @@ def collect_tweets_keyword():
         print(success_str)
         return '<h3>'+success_str+'</h3><br/>'+(twit_df.sample(5)[["author", "platform", "content"]]).to_html()
 
+@app.route('/fetchTweetsLocation', methods=['GET'])
+def collect_tweet_location():
+    # collecting args of GET request to get start and end dates of tweets to be collected
+    args = request.args
+    end_date = datetime.strptime(args.get('amp;end_date'), "%Y-%m-%d") # amp; is a temporary fix due to string conversion errors
+    start_date = datetime.strptime(args.get('start_date'), "%Y-%m-%d")
+    print('Extraction of tweet by location started')
+
+    # from here - move to a concurrent thread using ThreadPoolExecutor and send output as log. Assign Job IDs?
+    # naming for file to store extraction from a certain date to a certain date
+    path_to_storage_file = path_to_new_extractions.format(
+        sm_type="tweet_keyword", from_date=start_date.timestamp(), to_date=end_date.timestamp())
+    twit_df = None
+    try:
+        twit_df = get_twitter_posts_with_location("atlanta",
+            path_to_keywords_yaml, path_to_twit_keys_yaml, start_date, end_date)
+        print("Dumping data to pkl")
+        f = open(path_to_storage_file, 'wb')
+        pickle.dump(twit_df, f)
+        f.close()
+    except:
+        tb = traceback.format_exc()
+        error_str = 'Twitter Location Fetch failed with error \n{tb}\n'.format(
+            tb=tb)
+        # find if the file was accidentally half-made and delete
+        print(error_str)
+        return error_str
+    else:
+        success_str = 'Wrote twitter location extraction from {from_date} to {to_date} in {fname}'.format(
+            from_date=start_date.strftime("%m/%d/%Y, %H:%M:%S"), to_date=end_date.strftime("%m/%d/%Y, %H:%M:%S"), fname=path_to_storage_file)
+        print(success_str)
+        return '<h3>'+success_str+'</h3><br/>'+(twit_df.sample(5)[["author", "platform", "content"]]).to_html()
 
 @app.route('/fetchInstaPosts', methods=['GET'])
 def collect_insta_keyword():
@@ -101,14 +142,18 @@ def collect_insta_keyword():
 
     # from here - move to a concurrent thread using ThreadPoolExecutor and send output as log. Assign Job IDs?
     # naming for file to store extraction from a certain date to a certain date
-    path_to_storage_file = path_to_new_extractions.format(
+    path_to_storage_file = path_to_pre_comment_extractions.format(
         sm_type="insta_post", from_date=start_date.timestamp(), to_date=end_date.timestamp())
     insta_df = None
     try:
         insta_df = get_insta_posts(
             path_to_keywords_yaml, path_to_ct_keys_yaml, start_date, end_date)
         f = open(path_to_storage_file, 'wb')
+        print("Dumping insta data to pkl 1")
         pickle.dump(insta_df, f)
+        print("Initiating insta comment pull")
+        collect_comments_post(insta_df,start_date,end_date)
+        print("Done pulling insta comments")
         f.close()
     except:
         tb = traceback.format_exc()
@@ -135,14 +180,18 @@ def collect_facebook_keyword():
 
     # from here - move to a concurrent thread using ThreadPoolExecutor and send output as log. Assign Job IDs?
     # naming for file to store extraction from a certain date to a certain date
-    path_to_storage_file = path_to_new_extractions.format(
+    path_to_storage_file = path_to_pre_comment_extractions.format(
         sm_type="fb_post", from_date=start_date.timestamp(), to_date=end_date.timestamp())
     fb_df = None
     try:
-        fb_df = get_insta_posts(
+        fb_df = get_fb_posts(
             path_to_keywords_yaml, path_to_ct_keys_yaml, start_date, end_date)
         f = open(path_to_storage_file, 'wb')
+        print("Dumping fb data to pkl 1")
         pickle.dump(fb_df, f)
+        print("Initiating fb comment pull")
+        collect_comments_post(fb_df,start_date,end_date)
+        print("Done pulling fb comments")
         f.close()
     except:
         tb = traceback.format_exc()
@@ -157,7 +206,19 @@ def collect_facebook_keyword():
         print(success_str)
         return '<h3>'+success_str+'</h3><br/>'+(fb_df.sample(5)[["author", "platform", "content"]]).to_html()
 
-
+def collect_comments_post(sm_df,start_date,end_date):
+    # append _comments to the file
+    i = 0
+    while i < sm_df.shape[0]:
+        print("comment pull number "+str(i))
+        comments_output = get_post_comments(sm_df[sm_df.platform!='twitter'].iloc[i:i+100], path_to_exportcomments_keys)
+        f = open(path_to_pre_comment_extractions.format(sm_type="sm_post_"+str(int(i/100)+1), from_date=start_date.timestamp(), to_date=end_date.timestamp()), 'wb')
+        pickle.dump(comments_output[0],f)
+        f.close()
+        f1 = open(path_to_pre_comment_extractions.format(sm_type="comment_post_"+str(int(i/100)+1), from_date=start_date.timestamp(), to_date=end_date.timestamp()), 'wb')
+        pickle.dump(comments_output[1],f1)
+        f1.close()
+        i+=100
 
 if __name__ == '__main__':
     app.run()
---
+++ exportcomments_fetch.py
@@ -0,0 +1,219 @@
+import pandas as pd
+import yaml
+import json
+import numpy as np
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from pathlib import Path
+import urllib
+import time
+
+from exportcomments import ExportComments
+# Documentation here: https://github.com/exportcomments/exportcomments-python
+
+# Inputs:
+
+# path_to_ex_keys_yaml: Path to API tokens: from home/'.exportcomments_keys.yaml' (which each user should have on their system, not synced on Dropbox)
+# posts_df: Dataframe with rows of all posts we'd like to pull comments for (function limits to FB and IG)
+
+# Outputs - as a tuple:
+
+# posts_df_comments: Same dataframe of posts inputted, with new/updated column commentCountExported
+# comments_df: Dataframe of comments data
+
+def get_post_comments(posts_df, path_to_ex_keys_yaml):
+    # SETUP
+    with open(path_to_ex_keys_yaml) as f:
+        ex_keys = yaml.safe_load(f)
+    
+    # TODO? -- depending on what data is updated/made
+    # Create column commentCountExported if not already present - with NA values
+    # Continue only with posts where commentCountExported is NA
+    
+    # Only do FB and IG posts
+    posts_df_fb_ig = posts_df[(posts_df.platform=='facebook') | (posts_df.platform=='instagram')]
+    
+    # Create comment exports
+    comments_exports = create_comment_exports(posts_df_fb_ig, ex_keys)
+    
+    print()
+    print('=====')
+    print()
+    
+    # Get the export data
+    comments_results = get_comment_exports(comments_exports, ex_keys)
+    
+    # Create a combined df of comments from all posts,
+    # including a var postIndex to keep track of which post it's for:
+    dfs = []
+    for i in comments_results:
+        # Convert comments for the post into a df
+        comment_df = pd.DataFrame(comments_results[i])
+
+        # Add column for post_id
+        comment_df['postIndex'] = i
+
+        # Move post_id column to the front
+        col = comment_df.pop('postIndex')
+        comment_df.insert(0, col.name, col)
+
+        # Add this df to the list of df's
+        dfs.append(comment_df)
+
+    # Concatenate all df's together
+    comments_df = pd.concat(dfs)
+    
+    # To add to posts_df: Column with count of comments for a post
+    commentCountExported = comments_df.groupby('postIndex')['commentId'].count().rename('commentCountExported')
+    posts_df_comments = posts_df.merge(commentCountExported, left_index=True, right_index=True, how='left')
+    posts_df_comments['commentCountExported'] = posts_df_comments.commentCountExported.fillna(0).astype(int)
+    
+    return posts_df_comments, comments_df
+
+# Inputs:
+
+# posts_df: Dataframe with rows of all posts we'd like to pull comments for (this will actually try it for every. single. post. -- so should be limited to FB/IG already)
+# ex_keys: Exportcomments API key
+
+# Outputs:
+
+# comments_exports: dict, with keys i = index of post in posts_df; object is r_export.body that contains info about the export created
+# (no item in dict created if the post does not have any comments to export - exportcomments gives an error saying so)
+
+def create_comment_exports(posts_df, ex_keys):
+    # Instantiate the client Using your API key
+    ex = ExportComments(ex_keys['exportcomments']['token'])
+    
+    print("STARTING: CREATE COMMENT EXPORTS")
+    
+    # Get start time
+    start = time.time()
+
+    # Initiate dict to collect comments export info for each post
+    comments_exports = dict()
+    
+    # Initiate posts-processed counter
+    p = 0
+
+    for i, post in posts_df.iterrows():
+        # Rate limit for Create the export: "You cannot create more that 20 resources within 3 minutes." = 9 seconds per request
+        time.sleep(9)
+        
+        # Just for a status update - print for every 50th post's comment fetched
+        p = p + 1
+        if p % 50 == 0:
+            print("Post #: " + str(p) + ", Post index: " + str(i) + ", Time: " + str(time.time() - start))
+
+        # While there is no export made yet - try and create the export:
+        export_made = False
+        while export_made == False:
+
+            # Create the export:
+            r_export = ex.exports.create(
+                url=post['url'],
+                replies='true' #replies='false'
+            )
+
+            # Check API return code for error
+            if r_export.body['code'] != 200:
+                print(i) # print post index for debugging
+                print(post['url']) # print post URL for debugging
+                print(r_export.body)
+                break
+
+            # Check for successful export creation
+            if 'guid' in r_export.body['data'].keys():
+                export_made = True
+                continue
+
+            else:                
+                # Check API response for rate-limit error notification:
+                if 'error_code' in r_export.body['data'].keys():
+#                     print(r_export.body)
+                    time.sleep(r_export.body['data']['seconds_to_wait'])
+                    continue # try again to make export
+                else:
+                    print(i) # print post index for debugging
+                    print(post['url']) # print post URL for debugging
+                    print(r_export.body)
+                    break
+
+        # Store the export info
+        if export_made == True:
+            comments_exports[i] = r_export.body
+
+    print()
+    print("Posts gone through:")
+    print(len(comments_exports))
+    print("Time taken:")
+    print(time.time() - start)
+    
+    return comments_exports
+
+# Inputs:
+
+# comments_exports: dict, as outputted by create_comment_exports
+# ex_keys: Exportcomments API key
+
+# Outputs:
+
+# comments_results: dict, with keys i = index of post in posts_df; object is resulting json of exported comment
+
+def get_comment_exports(comments_exports, ex_keys):
+    # Instantiate the client Using your API key
+    ex = ExportComments(ex_keys['exportcomments']['token'])
+    
+    print("STARTING: GET COMMENT EXPORTS")
+
+    # Get start time
+    start = time.time()
+
+    # Initiate dict to collect comments data for each post
+    comments_results = dict()
+    
+    # Initiate posts-processed counter
+    p = 0
+
+    for i, r_export in comments_exports.items():
+        # Overall rate limit: ExportComments.com limits API usage to 5 requests per second, with any requests thereafter being queued up to a maximum of 5 requests.
+        time.sleep(0.2)
+        
+        # Just for a status update - print for every 50th post's comment fetched
+        p = p + 1
+        if p % 50 == 0:
+            print("Post #: " + str(p) + ", Post index: " + str(i) + ", Time: " + str(time.time() - start))
+
+        # Check the export status: "For each run, you may make at most 25 calls during the first 5 minutes after the export started." = 12 seconds per request
+        export_status = "not started"
+        while export_status != "done":
+            r_check = ex.exports.check(
+                guid=r_export['data']['guid']
+            )
+            export_status = r_check.body['data'][0]['status']
+            if export_status == "error":
+                # Print out post export info for debugging, if it's not a "no comments found" situation
+                if not r_check.body['data'][0]['error'].startswith("No comments"):
+                    print(i) # print post index for debugging
+                    print(r_export['data']['url']) # print post URL for debugging
+                    print(r_check.body)
+                break
+            if export_status != "done":
+                time.sleep(12)
+
+        # Once export status is good...
+        if export_status == 'done':
+            # Open result json
+            r_result = urllib.request.Request('https://www.exportcomments.com' + r_export['data']['rawUrl'], headers={'User-Agent' : "Magic Browser"}) # header is to avoid 403 error...
+            result_f = urllib.request.urlopen(r_result)
+            result = result_f.read()
+            # Add to results dict
+            comments_results[i] = json.loads(result)
+
+    print()
+    print("Comment exports gone through:")
+    print(len(comments_exports))
+    print("Time taken:")
+    print(time.time() - start)
+    
+    return comments_results
